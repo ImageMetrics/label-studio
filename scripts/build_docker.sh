f0643388bf68b0395101c50f4a81d9e356b7e52d
Christopher De Jesus <christopher.dejesus@image-metrics.com>
2023-08-28 17:46:26 -0400

    test this in remote

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,5 +1,7 @@
+python3 -m venv env
+source env/bin/activate
+python -m pip install label-studio
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
 if bash ${SCRIPT_DIR}/../deploy/prebuild.sh; then
   docker build -t heartexlabs/label-studio ${SCRIPT_DIR}/..
 fi
