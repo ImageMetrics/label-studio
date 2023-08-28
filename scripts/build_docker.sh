08d1a1588b414a4bb3497456f79aabc636b815d2
Christopher De Jesus <christopher.dejesus@image-metrics.com>
2023-08-28 17:50:37 -0400

    do the install in it instead of virtualenv

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,6 +1,4 @@
-python3 -m venv env
-source env/bin/activate
-python -m pip install label-studio
+python3 -m pip install label-studio
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if bash ${SCRIPT_DIR}/../deploy/prebuild.sh; then
   docker build -t heartexlabs/label-studio ${SCRIPT_DIR}/..
