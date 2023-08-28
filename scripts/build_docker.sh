12b80d0f891063ea7eddea7613d24a0446dc74ef
Christopher De Jesus <christopher.dejesus@image-metrics.com>
2023-08-28 17:55:28 -0400

    install it i guess

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,3 +1,4 @@
+pip install django label-studio
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if bash ${SCRIPT_DIR}/../deploy/prebuild.sh; then
   docker build -t heartexlabs/label-studio ${SCRIPT_DIR}/..
